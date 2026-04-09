3e87186433ba57f2f42b35dacc71aa86d649d52a
Hamza Aitaabella <haitaabe@e2r1p12.1337.ma>
2026-04-09 19:59:41 +0100

    .

--- src_channels/managerchannel.cpp
+++ src_channels/managerchannel.cpp
@@ -6,7 +6,20 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 16:38:58 by haitaabe          #+#    #+#             */
-/*   Updated: 2026/04/09 16:38:59 by haitaabe         ###   ########.fr       */
+/*   Updated: 2026/04/09 19:59:28 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "../includes_channels/channel.hpp"
+#include "../includes_channels/managerchannel.hpp"
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+
+void managerchannel::handle_input(const std::string &input, client &c)
+{
+    
+}
