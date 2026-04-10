6158ddfca61f311a019ff3162fe88ef155305593
Hamza Aitaabella <haitaabe@e2r1p12.1337.ma>
2026-04-10 14:57:28 +0100

    finishing PASS

--- src_channels/managerchannel.cpp
+++ src_channels/managerchannel.cpp
@@ -6,7 +6,7 @@
 /*   By: haitaabe <haitaabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 16:38:58 by haitaabe          #+#    #+#             */
-/*   Updated: 2026/04/09 19:59:28 by haitaabe         ###   ########.fr       */
+/*   Updated: 2026/04/10 14:56:34 by haitaabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,43 @@
 
 void managerchannel::handle_input(const std::string &input, client &c)
 {
+    std::istringstream check_input(input);
+    std::string token;
+
+    std::vector<std::string> tokens;
+    while (check_input >> token)
+    {
+        tokens.push_back(token);
+    }
+
+    if (tokens.empty())
+        return ;
+
+    if (!c.regestred)
+    {
+        if(tokens[0] == "PASS")
+        {
+            if (tokens.size() > 1)
+                handlePass(&c, tokens[1]);
+        }
+    }
+}
+
+void managerchannel::handlePass(client *c, const std::string &password)
+{
+    if (c->regestred)
+    {
+        std::cout << "Error: User is already registered!" << std::endl;
+        return;
+    }
+
+    if (password == _serverPassword)
+    {
+        c->pass_ok = true;
+    }
     
+    else 
+    {
+        c->pass_ok = false;
+    }
 }
